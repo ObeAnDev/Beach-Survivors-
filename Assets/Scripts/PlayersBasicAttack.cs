342814d17c2551c04d62345acd7cb3330081ef20
ObeAnDev <obean2000@gmail.com>
2025-12-26 01:20:03 +0200

    Spelling

--- Assets/Scripts/PlayersBasicAttack.cs
+++ Assets/Scripts/PlayersBasicAttack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayersBasicAttak : MonoBehaviour
+public class PlayersBasicAttack : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
