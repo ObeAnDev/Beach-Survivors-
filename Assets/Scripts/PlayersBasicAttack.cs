65ce2567d5d071b047919c6503ab59b072eeef0e
ObeAnDev <obean2000@gmail.com>
2026-01-15 18:23:51 +0200

    OverlapSphere

--- Assets/Scripts/PlayersBasicAttack.cs
+++ Assets/Scripts/PlayersBasicAttack.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayersBasicAttack : MonoBehaviour
 {
     public int damage;
     public float attackRange;
 
+    public LayerMask enemyLayer;
 
     void Start()
     {
@@ -16,6 +19,39 @@ public class PlayersBasicAttack : MonoBehaviour
     
     void Update()
     {
+        EnemyReach();
+    }
+    public void Attack()
+    {
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+    public void EnemyReach()
+    {
+        List<Collider> collider = Physics.OverlapSphere(transform.position, attackRange, enemyLayer).ToList<Collider>(); 
+
+        foreach (var reached in collider)
+        {
+            if (reached.gameObject.CompareTag("Enemy"))
+            {
+                Debug.Log("In sphere");
+                Attack();
+            }
+        }
+            
         
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            
+        }
     }
 }
