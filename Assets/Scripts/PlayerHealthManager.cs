20e60d7b39268a963982894574176ef506cd2e4f
ObeAnDev <obean2000@gmail.com>
2026-02-04 23:44:19 +0100

    Added health sys to player

---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    [SerializeField] float health;
+    public float Health => health;
+    public void TakeDamage(float _damage)
+    {
+        if (_damage <= 0) return;
+
+        health -= _damage;
+
+        if (health < 0)
+            health = 0;
+
+        if (health == 0)
+            Die();
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
